191d3198d21e9127e09fef5f5c5a817ed6fc1f23
xkevin190 <kevinvelasco193@gmail.com>
2019-08-29 19:10:00 -0400

    continue the error with crypto

--- src/store/chats/chatAction.js
+++ src/store/chats/chatAction.js
@@ -1,13 +1,5 @@
 import AsyncStorage from "@react-native-community/async-storage";
-import { socket } from "../../utils/socket";
 import { ActionTypes } from "../constants";
-
-// export const initialChat = data => dispatch => {
-//   socket.collection("chats").add({
-//     ...data
-//   });
-// };
-
 
 export const initialChat = (data) => dispatch =>{
   dispatch({
--- src/utils/socket.js
+++
@@ -1,5 +0,0 @@
-import firebase from 'react-native-firebase'
-
-export const socket = firebase.firestore()
-
-
